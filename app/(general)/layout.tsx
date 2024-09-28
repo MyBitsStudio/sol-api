35cff59681852f770fbb958f4faf66ff4ac4120d
MyBits <mybitsstudiogaming@gmail.com>
2024-09-28 11:17:55 -0700

    Metadata

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,9 +1,32 @@
 import {ReactNode} from "react";
 import {SiteHeader} from "@/components/layout/header";
 import {ToastContainer} from "react-toastify";
+import {Metadata} from "next";
 
 interface RootLayoutProps {
     children: ReactNode
+}
+
+export const metadata: Metadata = {
+    title: 'WIF Trending',
+    description:
+        'WIF Trending is the king of trending on SOL',
+    twitter: {
+        title: 'WIF Trending',
+        description:
+            'WIF Trending is the king of trending on SOL',
+        images: '/gifs/WifBuy.mp4',
+        card: 'summary_large_image',
+        site: '@WIFTRENDING',
+    },
+    openGraph: {
+        title: 'WIF Trending',
+        description:
+            'WIF Trending is the king of trending on SOL',
+        images: '/gifs/WifBuy.mp4',
+        url: 'https://www.wiftrending.app/',
+        type: 'website',
+    },
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
